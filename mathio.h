f15f02c2a26169cc90885d8f29b7e58289682635
Giulianos <scaglionigiuliano@gmail.com>
2020-03-29 04:29:15 -0300

    add cursor functions to MathIO contract

--- mathio.h
+++ mathio.h
@@ -6,10 +6,22 @@
 class MathIO
 {
 public:
+  enum class CursorDir {
+	  Up,
+	  Down,
+	  Left,
+	  Right,
+  };
+
   virtual int getWidth()                    = 0;
   virtual int getHeight()                   = 0;
   virtual void render(ScreenBuffer* buffer) = 0;
   virtual void setCompactMode(bool compact) = 0;
+  virtual void enableCursor(bool enable) = 0;
+  // Returns whether wrapped around or not
+  virtual bool moveCursor(CursorDir direction) = 0;
+  virtual void toggleCursorVisibility() = 0; 
+  virtual void forceCursorShow() = 0;
 };
 
 #endif
